9cec3e078759ad88f807d5a7ae08a3f57c763f60
Adarsh Rawat <00adarshrawat00@gmail.com>
2024-04-29 15:24:45 +0530

    adding error handling

--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Response, Request } from 'express'
 export const app = express();
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
-
+import {ErrorMiddleware} from './middleware/error';
 
 // body parser
 app.use(express.json({limit: "50mb"}))
@@ -23,6 +23,8 @@ app.get('/test', (req:Request, res:Response, next:NextFunction)=>{
 // unknow routes
 app.get("*",(req:Request, res:Response, next:NextFunction)=>{
     const err = new Error(`Route ${req.originalUrl} not found`) as any;
-    err.statusCode = 404;
+    err.statusCode = 404; 
     next(err);
 })
+
+app.use(ErrorMiddleware)
